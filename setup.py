1461c94b2e76923cdc9ae96d167aa68360690231
Forever-CodingNoob <johnnylin6256@gmail.com>
2020-06-14 12:25:46 +0800

    add run() to the instance of BrainF

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="BrainpyFuck",
-    version="0.0.3",
+    version="0.0.4",
     author="ForeverCdgNoob",
     author_email="johnnylin6526@gmail.com",
     description="A small brainfuck interpreter for python, including prettyprint inside",
