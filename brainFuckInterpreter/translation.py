be879e4c0ef9f3a711364f13c28b1f0c371ae700
Forever-CodingNoob <johnnylin6256@gmail.com>
2021-01-30 21:13:00 +0800

    add simplifier

--- brainFuckInterpreter/translation.py
+++ brainFuckInterpreter/translation.py
@@ -1,8 +1,17 @@
 from .fuck import BrainF
+import re
 # import sys
 # sys.setrecursionlimit(10**4)
 TEXT="""WOW! Is the flower very beautiful! very pretty! The flower have blue, tiffany blue, yellow, no green and red, this is very bad colour. Can you see? Have people on the flower, this is very bad, is very NO OK! The law are the very sad, don't small. Can you hear? Is flower sad, it very sad, is very NO OK!
 I am people, I can see and hear, I see people on the flower, I hear flower sad, I am very angry! Oh NO! That not people, that no heart, that is monkey, that is zoo's animal, that are very bad, is very NO OK! I am people, I love flower and all people, I am very good, My friend is Frankin, not on the flower's monkey or zoo's animal."""
+def simplify(func):
+    def wrapped(*args,**kwargs):
+        code=func(*args,**kwargs)
+        while code.find('><')!=-1 or code.find('<>')!=-1:
+            code=re.sub('><|<>','',code)
+        return code
+    return wrapped
+@simplify
 def text2bf(text,print_output=False,limit_a=2,limit_b=10):
     assert (limit_b>=limit_a),'error!'
     _ascii = [ord(i) for i in text]
@@ -37,7 +46,7 @@ def text2bf(text,print_output=False,limit_a=2,limit_b=10):
     if print_output:
         print()
     return code
-
+@simplify
 def text2bf2(text,print_output=False,limit=2):
     _ascii = [ord(i) for i in text]
     if print_output:
@@ -76,7 +85,7 @@ def text2bf2(text,print_output=False,limit=2):
         print()
     return code
 
-
+@simplify
 def text2bf3(text, print_output=False, limit=2):
     _ascii = [ord(i) for i in text]
     if print_output:
---
+++ demo4.py
@@ -0,0 +1,4 @@
+from brainFuckInterpreter import TEXT,text2bf,text2bf2,text2bf3
+print(text2bf2(TEXT,print_output=True,limit=2))
+# print(text2bf(TEXT,print_output=True,limit_a=2,limit_b=10))
+# print(text2bf3(TEXT,print_output=True,limit=2))
